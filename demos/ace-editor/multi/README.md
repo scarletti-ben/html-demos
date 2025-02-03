ec83cdd72e1857bbc1b9453e6f4b9bda2e006c0e
scarletti.ben@gmail.com <scarletti.ben@gmail.com>
2025-02-03 18:00:43 +0000

    tweaks

---
+++ demos/ace-editor/multi/README.md
@@ -0,0 +1,20 @@
+# Ace Editor
+
+# Issues
+- Autcomplete and snippets popup are not correctly aligned
+- Selection and cursor remain on inactive editors
+- Horizontal scrollbars in the editors randomly, often past line 10 when the gutter resizes
+
+# TODO
+- Save editor settings to `local storage`
+- Save editor text to `local storage`
+- F11 fullscreen for a single editor doesn't work in ACTUAL fullscreen
+- Learn file tabs for a single editor
+
+### Hotkeys
+- `Control + ,` for opening the tools panel
+- `F1`
+- `Control + D`
+- `Control + L`
+- `Control + F`
+- `Control + H`
--- demos/ace-editor/multi/script.js
+++ demos/ace-editor/multi/script.js
@@ -1,6 +1,8 @@
 // Variables, Constants and Declarations
 let pageContainer = document.getElementById('page');
+let newButton = document.getElementById("new-button");
 let editorCount = 0;
+let longPressTimer = null;
 const defaultText = `
 
 def test(value: int) -> int:
@@ -11,6 +13,30 @@ value = test(8)
 print(value)
 
 `
+
+// Long press on new-button to open settings menu
+newButton.addEventListener("touchstart", () => {
+    let id = `editor_${editorCount}`;
+    longPressTimer = setTimeout(() => openSettingsMenu(id), 1000);
+});
+
+// Long press on new-button to open settings menu
+newButton.addEventListener("touchend", () => {
+    clearTimeout(longPressTimer);
+});
+
+// Long press on new-button to open settings menu
+newButton.addEventListener("touchmove", () => {
+    clearTimeout(longPressTimer);
+});
+
+// Open the settings menu for a given editor id
+function openSettingsMenu(id) {
+    let editor = ace.edit(id);
+    editor.execCommand("showSettingsMenu");
+    // editor.execCommand("showKeyboardShortcuts");
+    // editor.showKeyboardShortcuts();
+}
 
 // Toggle the cursor for a given editor id
 function toggleCursor(id) {
@@ -23,25 +49,18 @@ function toggleCursor(id) {
     }
 }
 
-// Function to add a spacer to pageContainer, <div class="spacer"></div>
-function addSpacer() {
-    var element = document.createElement('div');
-    element.classList.add('spacer');
-    pageContainer.appendChild(element);
-}
-
 // Function to add a new ace editor to pageContainer, with incremented id
 function addEditor() {
+    
     let id = `editor_${++editorCount}`;
     var element = document.createElement('pre');
     element.id = id;
-    pageContainer.appendChild(element);
+    pageContainer.insertBefore(element, newButton)
     let editor = ace.edit(id);
     editor.session.setMode("ace/mode/python");
     editor.renderer.setScrollMargin(16, 0);
     editor.setShowPrintMargin(false);
     editor.setShowInvisibles(false);
-    // editor.renderer.setPadding(8);
     editor.session.setOption("wrap", true);
     editor.setOption("displayIndentGuides", false)
     editor.setOption("showInvisibles", false);
@@ -49,22 +68,34 @@ function addEditor() {
     editor.session.setUseSoftTabs(true);
     editor.setOption("highlightActiveLine", false);
     editor.setOption("highlightGutterLine", false);
-    editor.setOption("showMatchingBracket", false);
-    // toggleCursor(id);
     editor.setOptions({
         autoScrollEditorIntoView: true,
+        // scrollPastEnd: 0.1,
         theme: "ace/theme/gruvbox",
         highlightSelectedWord: false,
+        enableBasicAutocompletion: true,
+        enableSnippets: true,
+        enableLiveAutocompletion: true
     });
+
+    editor.container.addEventListener('keydown', function(event) {
+        if (event.key === 'F11') {
+            event.preventDefault();
+            const isFullScreen = document.body.classList.toggle('fullScreen');
+            editor.container.classList.toggle('fullScreen', isFullScreen);
+            editor.setAutoScrollEditorIntoView(!isFullScreen);
+            editor.resize();
+        }
+    });
+
     editor.setValue(defaultText.trim());
     editor.clearSelection();
+    pageContainer.scrollBy(0, 1000);
+
 }
 
 // Main function for script.js
 function main() {
-    addSpacer();
-    addEditor();
-    addSpacer();
     addEditor();
 }
 main()
